a9d7986db6998ea33e07e9dc5c868eff053b08e7
Alexandre Lim <alexandre.lim@abbeal.com>
2018-06-20 23:40:09 +0200

    feat(persevere): add persevere skill page

---
+++ src/pages/Persevere.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Row } from 'antd';
+
+const Persevere = () => (
+  <Row className= "content-row">
+    <Row type="flex" justify="center">
+      <h1>Persévérer pour réussir</h1>
+    </Row>
+    <Row type="flex" className="skill-definition">
+      <p>
+        <span className="span-bold">Définition</span> : Faire face à l'adversité
+        tout en préservant son intégrité pour atteindre un objectif.
+      </p>
+    </Row>
+    <Row type="flex" className="skill-introduction">
+      <p>
+        Pourquoi persévérer ? Ma réponse est que persévérer est un des facteurs qui permet la réussite personnelle
+        et professionnelle. Cette qualité peut se travailler à chaque instant et j'ai fait le choix de l'intégrer
+        dans ma routine quotidienne.
+      </p>
+      <p>
+        Elle m'appuie dans mon métier de développeur web.
+        Le monde numérique évolue constamment et de nouveaux challenges apparaissent.
+        Face à ce monde changeant et à la compétition grandissante, il est devenu indispensable de pratiquer
+        la veille technologique et de se former en continue.
+      </p>
+      <p>
+        Persévérer dans le domaine du web entraîne une rigueur dans l'écriture du code et le respect des bonnes pratiques
+        qui faciliteront la maintenance applicative. Les solutions recherchées sont approfondies et ne tombent
+        pas dans la facilité d'une réponse trouvée sur le net
+      </p>
+    </Row>
+    <Row type="flex">
+      <h2>Persévérer dans le domaine sportif</h2>
+    </Row>
+    <Row type="flex" className="">
+      <p>
+        J'ai pu mettre en pratique cette aptitude et en voir ses résultats plus que probant.
+      </p>
+      <p>
+        Lors de mes 15 ans, j'ai rejoint le club de Volley-Ball en section compétition.
+        À mes débuts, cela a été extrêmement difficile. Le coach me réprimandait et je sentais que je ralentissais
+        le flow d'entrainement de l'équipe déjà de haut niveau. J'étais le seul nouveau, le restant de l'équipe
+        avait au minimum 2-3 ans d'entrainement derrière eux. Je me rappelle encore du moment où je n'arrivais
+        pas à servir et que le coach a attendu jusqu'à que je passe cinq services d'affilé qu'il jugeait correct.
+        Sous pression, j'ai pensé à lâcher la balle et m'en aller, mais j'ai serré les dents et relevé ce challenge.
+      </p>
+      <p>
+        Malgré ma forte timidité et ma petite stature, j'ai su insister et contre toute attente, atteindre le niveau régional
+        au bout de 3 années d'entrainements. Au cours de ces 3 années, nous avons remporté à plusieurs reprises le championnat
+        départemental et régional. Nous avons aussi atteint le quart de finale en Coupe de France.
+      </p>
+      <p>
+        Cette riche expérience et ces résultats positifs m'ont fait grandir. Ils m'ont ensuite poussé vers un autre sport,
+        le Badminton. Pratiquant cette activité en compétition depuis 6 ans, j'ai pu une nouvelle fois accéder au niveau régional
+        et avoir une expérience en tant qu'entraineur.
+      </p>
+      <p>
+        Mes objectifs ont été accomplis et j'en tire une grande satisfaction.
+        Je peux maintenant envisager de plus grands objectifs encore impensables lors de mes débuts tel que les nationales.
+      </p>
+      <p>
+        Dans l'atteinte de mes objectifs, la notion de persévérance s'est traduite par une régularité des entrainements,
+        un dépassement constant de soi et l'écoute des conseils du coach et de mes coéquipiers.
+        Des études théoriques et stratégiques en dehors du terrain ont été nécessaires et il a fallu supporter la forte exigence
+        du coach et de l'équipe.
+      </p>
+    </Row>
+    <Row type="flex">
+      <h2>Un pas dans le domaine professionnel</h2>
+    </Row>
+    <Row type="flex">
+      <p>
+        Le monde professionnel ne diffère pas tant que ça du domaine sportif. Cette qualité peut s'avérer d'une grande aide
+        notamment dans les situations les plus difficiles.
+      </p>
+      <p>
+        Ma persévérance au sein de mes expériences en entreprise m'a permis un rendu de travail de qualité et peu de retour
+        de bugs du client au cours de mon passage sur les projets ISIDOR et Natura 2000 (liens à mettre en place).
+        Des solutions efficaces et durables ont été mises en place pour répondre aux problèmes rencontrés.
+      </p>
+      <p>
+        ISIDOR et Natura 2000 sont des projets en TMA conséquentes et existent depuis plus de 5 ans.
+        La base de code est énorme, les fonctionnalités nombreuses et le système complexe. Face à de tel projet,
+        il faut de la patience et ne pas abandonner face aux bugs qui peuvent paraître impossible à trouver ou à résoudre.
+      </p>
+      <p>
+        Face aux obstacles et à l'adversité, je choisi de persévérer car je suis persuadé que
+        ce sont les petits efforts de tous les jours qui font la différence.
+      </p>
+    </Row>
+    <Row type="flex">
+      <h2>Et aujourd'hui ?</h2>
+    </Row>
+    <Row type="flex">
+      <p>
+        Cette capacité à persévérer est essentielle dans toutes sortes de situation que je vis au quotidien.
+        Le revers de la médaille est de ne pas savoir quand s'arrêter. A trop vouloir aller jusqu'au bout,
+        sans écouter son entourage, son corps et son esprit peut amener à des dommages irréversibles comme un burnout.
+        Le mélange passion et persévérance peut provoquer un cocktail explosif.
+        Il me faut sans cesse me rappeler de trouver un juste milieu et quelques fois dire stop ou pivoter afin
+        d'avancer dans mes objectifs et même dans la vie.
+      </p>
+    </Row>
+  </Row>
+);
+
+export default Persevere;
