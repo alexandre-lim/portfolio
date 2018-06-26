387062816bb99d4372e43f69214b5daa1403a996
Alexandre Lim <alexandre.lim@abbeal.com>
2018-06-26 17:03:46 +0200

    style(home): add max width and center on quote card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const About = (props) => (
   <Row className= "content-row" type="flex" align="middle">
-    <Row type="flex" justify="center" style={{ flexGrow: 1 }}>
+    <Row type="flex" justify="center" style={{ flexGrow: 1 }} className="quote-content">
       <Col span={12}>
         <Card className="card-quote">
           <q>
