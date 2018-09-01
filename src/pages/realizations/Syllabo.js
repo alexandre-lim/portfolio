31ec7b91a26452cd9e650f95a99cdbfcaef2af7f
Alexandre Lim <alexandre.lim@abbeal.com>
2018-09-01 18:02:36 +0200

    refactor(realization): add refacto to Syllabo

--- src/pages/realizations/Syllabo.js
+++ src/pages/realizations/Syllabo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Tooltip } from 'antd';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import persevere from '../../persevere.png';
 import integrity from '../../integrity.png';
 import empathy from '../../empathy.png';
@@ -13,7 +13,7 @@ import practice from '../../practice.png';
 import database from '../../database.png';
 
 const Syllabo = () => (
-  <Row className= "content-row">
+  <Row className="content-row">
     <Row type="flex" justify="center">
       <h1>Syllabo</h1>
     </Row>
@@ -23,15 +23,20 @@ const Syllabo = () => (
       </Row>
       <Row type="flex" className="">
         <p>
-          Abbeal Valley, c'est l'opportunité qu'offre Abbeal de mener à bien des produits en interne.
-          Les produits sont proposés par les collaborateurs.  Une première présélection est réalisée afin de
-          déterminer les propositions de produits les plus aboutis. Une fois cette première étape passée, la personne
-          portant le produit devient <span className="span-bold">Product Owner.</span> Abbeal se charge des différents aspects extérieurs comme les
-          partenariats avec différentes écoles qui participeront au développement du projet.
+          Abbeal Valley, c'est l'opportunité qu'offre Abbeal de mener à bien des
+          produits en interne. Les produits sont proposés par les
+          collaborateurs. Une première présélection est réalisée afin de
+          déterminer les propositions de produits les plus aboutis. Une fois
+          cette première étape passée, la personne portant le produit devient{' '}
+          <span className="span-bold">Product Owner.</span> Abbeal se charge des
+          différents aspects extérieurs comme les partenariats avec différentes
+          écoles qui participeront au développement du projet.
         </p>
         <p>
-          Syllabo est un des projets retenu. Dans un premier temps, le produit a été développé par un groupe d'étudiants.
-          J'intègre Abbeal Valley afin de pousser le produit <span className="span-bold">jusqu'aux étoiles.</span>
+          Syllabo est un des projets retenu. Dans un premier temps, le produit a
+          été développé par un groupe d'étudiants. J'intègre Abbeal Valley afin
+          de pousser le produit{' '}
+          <span className="span-bold">jusqu'aux étoiles.</span>
         </p>
       </Row>
       <Row type="flex">
@@ -39,9 +44,12 @@ const Syllabo = () => (
       </Row>
       <Row type="flex" className="">
         <p>
-          Syllabo est une <span className="span-bold">application ludo-éducative</span> qui propose de se placer en complément du système scolaire pour
-          des enfants du primaire. L'enfant vivra une immersion culturelle et sera plongé dans un environnement
-          historique en commençant par la Grèce antique. Syllabo proposera ensuite des éléments ludiques,
+          Syllabo est une{' '}
+          <span className="span-bold">application ludo-éducative</span> qui
+          propose de se placer en complément du système scolaire pour des
+          enfants du primaire. L'enfant vivra une immersion culturelle et sera
+          plongé dans un environnement historique en commençant par la Grèce
+          antique. Syllabo proposera ensuite des éléments ludiques,
           mathématiques, scientifiques, géographiques et littéraires.
         </p>
       </Row>
@@ -50,91 +58,195 @@ const Syllabo = () => (
       </Row>
       <Row type="flex">
         <p>
-          Mon aventure avec <span className="span-bold">Beneylu</span> s'est terminée et me voici à développer pour ce magnifique produit qu'est Syllabo.
-          Cette fois, je pars presque de zéro et quel bonheur ! Je vis en ce moment mon rêve car je ressens les mêmes émotions
-          que lorsque j'étais sur <span className="span-bold">It'School.</span> Mon équipe n'est certes pas là, mais ce que je veux apporter à l'aide mon métier,
+          Mon aventure avec <span className="span-bold">Beneylu</span> s'est
+          terminée et me voici à développer pour ce magnifique produit qu'est
+          Syllabo. Cette fois, je pars presque de zéro et quel bonheur ! Je vis
+          en ce moment mon rêve car je ressens les mêmes émotions que lorsque
+          j'étais sur <span className="span-bold">It'School.</span> Mon équipe
+          n'est certes pas là, mais ce que je veux apporter à l'aide mon métier,
           mon but et mon rêve se retrouvent au travers de Syllabo.
         </p>
         <p>
-          Comme pour Beneylu, un nouveau challenge se présente car je dois <span className="span-bold">monter en compétence </span>
+          Comme pour Beneylu, un nouveau challenge se présente car je dois{' '}
+          <span className="span-bold">monter en compétence </span>
           sur du
-          <a target="_blank" rel="noopener noreferrer" href="https://nodejs.org/en/about/"> Node.js </a>avec
-          <a target="_blank" rel="noopener noreferrer" href="http://expressjs.com/"> Express.js </a>ainsi que sur du
-          <a target="_blank" rel="noopener noreferrer" href="https://www.mongodb.com/"> MongoDB. </a>Mais je dois aussi m'approprier
-          <a target="_blank" rel="noopener noreferrer" href="https://reactjs.org/"> React </a> avec
-          <a target="_blank" rel="noopener noreferrer" href="https://redux.js.org/"> Redux </a> et
-          <a target="_blank" rel="noopener noreferrer" href="https://redux-saga.js.org/"> Saga. </a>
-          Fort de mes expériences précédentes, je me dis que je peux le faire.
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://nodejs.org/en/about/"
+          >
+            {' '}
+            Node.js{' '}
+          </a>
+          avec
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="http://expressjs.com/"
+          >
+            {' '}
+            Express.js{' '}
+          </a>
+          ainsi que sur du
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.mongodb.com/"
+          >
+            {' '}
+            MongoDB.{' '}
+          </a>
+          Mais je dois aussi m'approprier
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://reactjs.org/"
+          >
+            {' '}
+            React{' '}
+          </a>{' '}
+          avec
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://redux.js.org/"
+          >
+            {' '}
+            Redux{' '}
+          </a>{' '}
+          et
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://redux-saga.js.org/"
+          >
+            {' '}
+            Saga.{' '}
+          </a>
+          Nous utilisons <span className="span-bold">Git</span> avec
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/"
+          >
+            {' '}
+            Github{' '}
+          </a>
+          pour le versioning. Fort de mes expériences précédentes, je me dis que
+          je peux le faire.
         </p>
         <p>
-          Nous appliquons la <span className="span-bold">méthode Kanban.</span> C'est encore nouveau et
-          j'apprécie les bienfaits de cette méthode qui pousse à travailler dans un environnement calme,
-          serein où l'on peut atteindre son plein potentiel. Nous utilisons <span className="span-bold">Git</span> avec
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/"> Github </a>pour le versioning.
+          Nous tentons de tirer le meilleur des méthodes agiles. Nous utilisons
+          la <span className="span-bold">méthode Kanban</span> qui se traduit
+          par l'utilisation du Board à l'aide de l'outil Jira. Nous utilisons
+          des méthodes agiles venant de <span className="span-bold">SCRUM</span>{' '}
+          comme le Daily Meeting les matins, la rétrospective agile ou une démo
+          produit qui ont lieu les jeudis midi. Nous appliquons aussi des
+          méthodes de l'
+          <span className="span-bold">XP (eXtreme Programming)</span> comme le
+          TDD, le Pair Programming ainsi que la Code Review. Toutes ces bonnes
+          pratiques et la superbe ambiance chez Abbeal Valley permettent la
+          qualité du produit et l'épanouissement de l'équipe.
         </p>
         <p>
-          J'ai eu l'occasion de présenter quelques démonstrations de l'application auprès des collègues
-          et ayant reçu de bon feedback, je pense être sur la bonne voie techniquement et humainement.
+          J'ai eu l'occasion de présenter quelques démonstrations de
+          l'application auprès des collègues et ayant reçu de bon feedback, je
+          pense être sur la bonne voie techniquement et humainement.
         </p>
         <p>
           Au moment où j'écris ces lignes, je développe encore sur Syllabo.
-          J'espère bien prolonger ce rêve jusqu'au moment où nous présenterons le produit aux enfants.
-          Et pourquoi pas continuer le produit plus loin et pouvoir le faire vivre financièrement.
+          J'espère bien prolonger ce rêve jusqu'au moment où nous présenterons
+          le produit aux enfants. Et pourquoi pas continuer le produit plus loin
+          et pouvoir le faire vivre financièrement.
         </p>
       </Row>
-      <br/>
+      <br />
       <Row type="flex" justify="center">
         <h2>Compétences</h2>
       </Row>
-      <br/>
+      <br />
       <Row type="flex" justify="center" gutter={16}>
-        <Link to="/skills/htmlCSS" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/htmlCSS"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Html5 & CSS3">
-            <img src={htmlCSS} alt="Html5 et CSS3" className="skills-button"/>
+            <img src={htmlCSS} alt="Html5 et CSS3" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/javascript" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/javascript"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Javascript">
-            <img src={javascript} alt="Javascript" className="skills-button"/>
+            <img src={javascript} alt="Javascript" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/webpack" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/webpack"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Webpack">
-            <img src={webpack} alt="Webpack" className="skills-button"/>
+            <img src={webpack} alt="Webpack" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/practice" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/practice"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="BDD et TDD">
-            <img src={practice} alt="BDD et TDD" className="skills-button"/>
+            <img src={practice} alt="BDD et TDD" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/database" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/database"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Base de données">
-            <img src={database} alt="Base de données" className="skills-button"/>
-          </Tooltip>
-        </Link>
-      </Row>
-      <br/>
-      <Row type="flex" justify="center" gutter={16}>
-        <Link to="/skills/persevere" onClick={() => this.handleHrefClick('skills')}>
-          <Tooltip title="Persévérer">
-            <img src={persevere} alt="Persévérer" className="skills-button"
+            <img
+              src={database}
+              alt="Base de données"
+              className="skills-button"
             />
           </Tooltip>
         </Link>
-        <Link to="/skills/integrity" onClick={() => this.handleHrefClick('skills')}>
+      </Row>
+      <br />
+      <Row type="flex" justify="center" gutter={16}>
+        <Link
+          to="/skills/persevere"
+          onClick={() => this.handleHrefClick('skills')}
+        >
+          <Tooltip title="Persévérer">
+            <img src={persevere} alt="Persévérer" className="skills-button" />
+          </Tooltip>
+        </Link>
+        <Link
+          to="/skills/integrity"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Intégrité">
-            <img src={integrity} alt="Intégrité" className="skills-button"/>
+            <img src={integrity} alt="Intégrité" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/empathy" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/empathy"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Empathie">
-            <img src={empathy} alt="Empathie" className="skills-button"/>
+            <img src={empathy} alt="Empathie" className="skills-button" />
           </Tooltip>
         </Link>
-        <Link to="/skills/teamwork" onClick={() => this.handleHrefClick('skills')}>
+        <Link
+          to="/skills/teamwork"
+          onClick={() => this.handleHrefClick('skills')}
+        >
           <Tooltip title="Travail d'équipe">
-            <img src={teamwork} alt="Travail d'équipe" className="skills-button"/>
+            <img
+              src={teamwork}
+              alt="Travail d'équipe"
+              className="skills-button"
+            />
           </Tooltip>
         </Link>
       </Row>
